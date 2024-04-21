62fbcd19d8d146f55fac8f60704f0c35cf3d6868
Vaishnavi-cyber-blip <72164140+Vaishnavi-cyber-blip@users.noreply.github.com>
2024-04-21 15:06:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,45 +1,157 @@
+import os
 import streamlit as st
-import requests
-import json
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_groq import ChatGroq
+from streamlit_chat import message
+
+load_dotenv()
+groq_api_key = os.environ["GROQ_API_KEY"]
 
 
-def make_post_request(api_key, query):
-    # URL with the channel token placeholder
-    url = 'https://payload.vextapp.com/hook/WSQJQZP1BG/catch/$(vaish18)'
+def initialize_session_state():
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
 
-    # Payload data
-    payload = {
-        "payload": query
-    }
+    if "generated" not in st.session_state:
+        st.session_state["generated"] = [
+            "Hi! Finley this side"
+        ]
 
-    # Headers
-    headers = {
-        'Content-Type': 'application/json',
-        'Apikey': 'Api-Key ' + api_key
-    }
-
-    # Make the POST request
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-
-    # Return response
-    return response
+    if "past" not in st.session_state:
+        st.session_state["past"] = ["Hey! 👋"]
 
 
-# Streamlit UI
-st.title("POST Request Application")
+def conversation_chat(query, chain, history):
+    result = chain({
+        "question": query,
+        "chat_history": history
+    })
+    history.append((query, result["answer"]))
+    return result["answer"]
 
-# Input fields for API Key and Query
-api_key = "zwIqDEt8.ZW5RfaXmjr2eKI7MZUIT2vl9tH5UEkhC"
-query = st.text_input("Enter your query:")
 
-# Button to trigger the request
-if st.button("Send Request"):
-    if api_key and query:
-        # Making the request
-        response = make_post_request(api_key, query)
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
 
-        # Displaying response
-        st.subheader("Response:")
-        st.json(response.json())
-    else:
-        st.write("Please enter API Key and Query.")
+    with container:
+        with st.form(key="my_form", clear_on_submit=True):
+            user_input = st.text_input(
+                "Query:",
+                placeholder="Ask me anything about OnFinance",
+                key="input"
+            )
+            submit_button = st.form_submit_button(label="Resolve")
+
+        if submit_button and user_input:
+            with st.spinner("Let me check ......"):
+                output = conversation_chat(
+                    query=user_input,
+                    chain=chain,
+                    history=st.session_state["history"]
+                )
+
+            st.session_state["past"].append(user_input)
+            st.session_state["generated"].append(output)
+
+    if st.session_state["generated"]:
+        with reply_container:
+            for i in range(len(st.session_state["generated"])):
+                message(
+                    st.session_state["past"][i],
+                    is_user=True,
+                    key=str(i) + "_user",
+                    avatar_style="initials",
+                    seed="ME"
+                )
+                message(
+                    st.session_state["generated"][i],
+                    key=str(i),
+                    avatar_style="initials",
+                    seed="OF"
+                )
+
+
+def create_conversational_chain(vector_store, model_name):
+    llm = ChatGroq(
+        temperature=0.5,
+        model_name=model_name,
+        groq_api_key=groq_api_key
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
+        memory=memory
+    )
+
+    return chain
+
+
+def select_model():
+    model_options = ["mixtral-8x7b-32768", "llama2-70b-4096"]
+    model_name = st.selectbox("Select a model", model_options)
+    return model_name
+
+
+def main():
+    initialize_session_state()
+    st.title("Firm Intelligence Bot")
+    # st.image("https://cdn.logojoy.com/wp-content/uploads/2018/05/30174816/8_big28.png", width=100)
+
+    # Load documents from a specific directory instead of user upload
+    document_directory = "C:\\Users\\265600\\PycharmProjects\\Firm_managment\\document_directory"
+    text = []
+    for filename in os.listdir(document_directory):
+        file_path = os.path.join(document_directory, filename)
+        file_extension = os.path.splitext(filename)[1]
+        if file_extension == ".pdf":
+            loader = PyPDFLoader(file_path)
+        elif file_extension == ".docx" or file_extension == ".doc":
+            loader = Docx2txtLoader(file_path)
+        elif file_extension == ".txt":
+            loader = TextLoader(file_path)
+        else:
+            continue
+
+        text.extend(loader.load())
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=768,
+        chunk_overlap=128,
+        length_function=len
+    )
+    text_chunks = text_splitter.split_documents(text)
+
+    embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"}
+    )
+
+    vector_store = Chroma.from_documents(
+        documents=text_chunks,
+        embedding=embedding,
+        persist_directory="chroma_store_groq"
+    )
+
+    model_name = select_model()
+    chain = create_conversational_chain(vector_store=vector_store, model_name=model_name)
+
+    display_chat_history(chain=chain)
+
+
+if __name__ == "__main__":
+    main()
